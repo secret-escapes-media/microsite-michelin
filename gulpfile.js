dc1aeb7047bbb2977b2732c74ef2cdc14901c63e
Alex Robertson-Brown <alex.robertsonbrown@gmail.com>
2018-11-13 15:13:00 +0000

    feedback changes

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ gulp.task('serve', ['build-jekyll'], function() {
     server: {
       baseDir: '_site/',
       routes: {
-        '/michelin-road-trips': '_site/'
+        '/unmissable-road-trips': '_site/'
       }
     }
   });
