39887a6d27ee8261231e299ab38fb493d3582d49
Alex Robertson-Brown <alex.robertsonbrown@gmail.com>
2018-11-14 15:10:20 +0000

    map tweaks

--- _assets/js/_components/map.js
+++ _assets/js/_components/map.js
@@ -11,8 +11,6 @@ $('.js-open-map').on('click', function(e) {
   // dont reload the map if its already open
   if ($('#map').children().length === 0) { map(selectedMapData); }
   $('.map__wrap').removeClass('is-closed').addClass('is-open');
-
-
 })
 
 function mapClose(e){
@@ -90,7 +88,13 @@ $(document).keyup(function(event) {
         method: 'GET',
         url: directionsRequest,
       }).done(function(data) {
+
+        // coordinates for entire route at a high detail
         var route = data.routes[0].geometry;
+        var routeStart = route.coordinates[0];
+        var routeEnd = route.coordinates.pop();
+
+        // draw route on map
         map.addLayer({
           id: 'route',
           type: 'line',
@@ -102,11 +106,49 @@ $(document).keyup(function(event) {
             }
           },
           paint: {
-            'line-width': 2,
+            'line-width': 1.33,
             'line-color': "#27509b"
           }
+        }, "road-label"); // this is the name of the layer that this new layer is getting added before
+        // // this lets you to see all of the layers in order in the console
+        // console.log(map.getStyle().layers);
+
+        map.addLayer({
+          id: 'route-start-and-end',
+          type: 'circle',
+          source: {
+            type: 'geojson',
+            data: {
+              type: "FeatureCollection",
+              features: [
+                {
+                  type: "Feature",
+                  geometry: {
+                    type: "Point",
+                    coordinates: routeStart
+                  }
+                },
+                {
+                  type: "Feature",
+                  geometry: {
+                    type: "Point",
+                    coordinates: routeEnd
+                  }
+                }
+              ]
+            }
+          },
+          paint: {
+            'circle-radius': 4,
+            'circle-color': '#ffffff',
+            'circle-stroke-width': 2,
+            'circle-stroke-color': '#27509b'
+          }
         });
+
       });
+
+
 
     // center the map on the route
     var bounds = new mapboxgl.LngLatBounds();
@@ -161,277 +203,6 @@ $(document).keyup(function(event) {
           'icon-allow-overlap': true
         }
       });
-
-
-
-
-
-      //////////////////////////////////////////////////////////////////////////
-      ////////////////////////////////////////////////////////////  CITY MARKERS
-      //////////////////////////////////////////////////////////////////////////
-
-      // // markers
-      // map.addLayer({
-      //   id: 'cities',
-      //   type: 'circle',
-      //   // Add a GeoJSON source containing place coordinates and information.
-      //   source: {
-      //     type: 'geojson',
-      //     data: cityMarkers
-      //   },
-      //   paint: { // style formatting for the cluster circle
-      //     "circle-color": markerColor,
-      //     "circle-radius": 10
-      //   }
-      // });
-
-      // // markers labels
-      // map.addLayer({
-      //   id: 'city-labels',
-      //   type: 'symbol',
-      //   source: {
-      //     type: 'geojson',
-      //     data: cityMarkers
-      //   },
-      //   layout: {
-      //     "text-field": "{title}",
-      //     "text-font": ["Rubik Regular"],
-      //     "text-size": 16,
-      //     "text-anchor": "left",
-      //     "text-letter-spacing": 0.2,
-      //     "text-transform": "uppercase",
-      //     "text-offset": [1.1, 0.1]
-      //   },
-      //   paint: { "text-color": "#2e2e2e" }
-      // });
-
-      // // maker label zoom levels
-      // map.setLayerZoomRange('city-labels', cityLabelZoomLevel, maxZoomLevel);
-      //
-      // function cityClickEvent(e) {
-      //
-      //   // find the coordinates for the clicked city
-      //   var clickedCityCoordinates;
-      //   for (var i = 0; i < cityMarkers.features.length; i++) {
-      //     if (cityMarkers.features[i].properties.id === e.features[0].properties.id) {
-      //       clickedCityCoordinates = cityMarkers.features[i].geometry.coordinates;
-      //     }
-      //   }
-      //
-      //   // get the current zoom level
-      //   var currentZoomLevel = map.getZoom();
-      //   if (currentZoomLevel <= stuttgartZoomLevel) {
-      //     // function for when zoomed out on germany
-      //     map.flyTo({
-      //       center: clickedCityCoordinates,
-      //       zoom: (stuttgartZoomLevel + .5)
-      //     });
-      //   } else {
-      //     var clickedModalId = e.features[0].properties.id;
-      //     modalOpen(null, clickedModalId);
-      //   }
-      // }
-
-      // // marker click event
-      // map.on('click', 'cities', function (e) {
-      //   cityClickEvent(e);
-      // });
-      //
-      // // maker label click event
-      // map.on('click', 'city-labels', function (e) {
-      //   cityClickEvent(e);
-      // });
-
-
-
-      //////////////////////////////////////////////////////////////////////////
-      ///////////////////////////////////////////////////////  STUTTGART MARKERS
-      //////////////////////////////////////////////////////////////////////////
-
-      // // markers
-      // map.addLayer({
-      //   id: 'stuttgart',
-      //   type: 'symbol',
-      //   source: {
-      //     type: 'geojson',
-      //     data: stuttgartMarker
-      //   },
-      //   layout: {
-      //     "icon-image": "poi-large",
-      //     "icon-size": .75,
-      //     'icon-anchor': "bottom",
-      //     "icon-allow-overlap": true
-      //   }
-      // });
-
-      // // marker zoom levels
-      // map.setLayerZoomRange('stuttgart', minZoomLevel, stuttgartZoomLevel);
-      //
-      // // marker label
-      // map.addLayer({
-      //   id: 'stuttgart-label',
-      //   type: 'symbol',
-      //   // Add a GeoJSON source containing place coordinates and information.
-      //   source: {
-      //     type: 'geojson',
-      //     data: stuttgartMarker
-      //   },
-      //   layout: {
-      //     "text-field": "{title}",
-      //     "text-font": ["Rubik Regular"],
-      //     "text-size": 20,
-      //     "text-anchor": "left",
-      //     "text-letter-spacing": 0.25,
-      //     "text-transform": "uppercase",
-      //     "text-offset": [1.35, -1.1]
-      //   },
-      //   paint: {
-      //     "text-color": "#2e2e2e"
-      //   }
-      // });
-
-      // // marker label zoom levels
-      // map.setLayerZoomRange('stuttgart-label', cityLabelZoomLevel, stuttgartZoomLevel);
-
-      // function focusOnStuttgart() {
-      //   var bounds = new mapboxgl.LngLatBounds();
-      //   poiMarkers.features.forEach(function(feature) {
-      //     bounds.extend(feature.geometry.coordinates);
-      //   });
-      //   // set different padding depending on original viewport width
-      //   // not super precise but should catch mobile phones and reduce the padding
-      //   var iconPadding;
-      //   if ($(window).innerWidth() < 400) {
-      //     iconPadding = { "padding": 30 };
-      //   } else {
-      //     iconPadding = { "padding": 60 };
-      //   }
-      //   map.fitBounds(bounds, iconPadding); // adds padding so markers aren't on edge
-      // }
-
-      // // marker click event
-      // map.on('click', 'stuttgart', function (e) {
-      //   focusMapOn(poiMarkers);
-      // });
-      //
-      // // marker label click event
-      // map.on('click', 'stuttgart-label', function (e) {
-      //   focusMapOn(poiMarkers);
-      // });
-
-
-
-      // //////////////////////////////////////////////////////////////////////////
-      // ////////////////////////////////////////////////////////////  POI CLUSTERS
-      // //////////////////////////////////////////////////////////////////////////
-      //
-      // // data source
-      // map.addSource("poi-markers", {
-      //   type: "geojson",
-      //   data: poiMarkers,
-      //   cluster: true,
-      //   clusterMaxZoom: 14,
-      //   clusterRadius: 45
-      // });
-      //
-      // // background colour
-      // map.addLayer({
-      //   id: "clusters",
-      //   type: "circle",
-      //   source: "poi-markers",
-      //   filter: ["has", "point_count"],
-      //   paint: {
-      //     "circle-color": markerColor,
-      //     "circle-radius": 23
-      //   }
-      // });
-      //
-      // // background colour zoom level
-      // map.setLayerZoomRange('clusters', stuttgartZoomLevel, maxZoomLevel);
-      //
-      // // background colour click event
-      // map.on('click', 'clusters', function (e) {
-      //   var features = map.queryRenderedFeatures(e.point, { layers: ['clusters'] });
-      //   var clusterId = features[0].properties.cluster_id;
-      //   map.getSource('poi-markers').getClusterExpansionZoom(clusterId, function (error, zoom) {
-      //     if (error) { return; }
-      //     // center on expanded cluster
-      //     map.flyTo({
-      //       center: features[0].geometry.coordinates,
-      //       zoom: (zoom + 1) // makes it zoom in a bit more so the group breaks up quicker?
-      //     });
-      //   });
-      // });
-      //
-      // // cluster number shadow
-      // map.addLayer({
-      //   id: "cluster-count-shadow",
-      //   type: "symbol",
-      //   source: "poi-markers",
-      //   filter: ["has", "point_count"],
-      //   layout: {
-      //     "text-field": "{point_count_abbreviated}",
-      //     "text-font": ["Rubik Medium"],
-      //     "text-size": 24
-      //   },
-      //   paint: {
-      //     "text-color": "#810612",
-      //     "text-translate": [-1.75,2.25]
-      //   }
-      // });
-      //
-      // // cluster number shadow zoom level
-      // map.setLayerZoomRange('cluster-count-shadow', stuttgartZoomLevel, maxZoomLevel);
-      //
-      // // cluster number
-      // map.addLayer({
-      //   id: "cluster-count",
-      //   type: "symbol",
-      //   source: "poi-markers",
-      //   filter: ["has", "point_count"],
-      //   layout: {
-      //     "text-field": "{point_count_abbreviated}",
-      //     "text-font": ["Rubik Medium"],
-      //     "text-size": 24
-      //   },
-      //   paint: {
-      //     "text-color": "#fff",
-      //     "text-translate": [-0.75,1.25]
-      //   }
-      // });
-      //
-      // // cluster number zoom level
-      // map.setLayerZoomRange('cluster-count', stuttgartZoomLevel, maxZoomLevel);
-
-
-
-      // //////////////////////////////////////////////////////////////////////////
-      // /////////////////////////////////////////////////////////////  POI MARKERS
-      // //////////////////////////////////////////////////////////////////////////
-      //
-      // // markers
-      // map.addLayer({
-      //   id: "pois",
-      //   type: "symbol",
-      //   source: "poi-markers",
-      //   filter: ["!has", "point_count"],
-      //   layout: {
-      //     "icon-image": "poi-small", // custom icon is in the mapbox style spritesheet
-      //     "icon-size": .75,
-      //     'icon-anchor': "bottom",
-      //     'icon-allow-overlap': true
-      //   }
-      // });
-      //
-      // // marker zoom level
-      // map.setLayerZoomRange('pois', stuttgartZoomLevel, maxZoomLevel);
-      //
-      // // marker click event
-      // map.on('click', 'pois', function (e) {
-      //   var clickedPoiId = e.features[0].properties.id
-      //   modalOpen(null, clickedPoiId);
-      // });
-
 
 
 
